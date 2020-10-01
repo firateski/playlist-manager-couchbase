6883c596e98c54cede2aba691c1c378dba41424d
Fırat Eşki <frat888@gmail.com>
2020-10-01 03:13:24 +0300

    Create playlist service

---
+++ src/main/java/com/firateski/playlist_manager/services/PlaylistService.java
@@ -0,0 +1,42 @@
+package com.firateski.playlist_manager.services;
+
+import com.firateski.playlist_manager.models.Playlist;
+import com.firateski.playlist_manager.models.Track;
+import com.firateski.playlist_manager.repositories.PlaylistRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlaylistService {
+
+    private final PlaylistRepository playlistRepository;
+
+    public PlaylistService(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
+    public void create(Playlist playlist) {
+        playlistRepository.createPlaylist(playlist);
+    }
+
+    public Playlist findById(String id){
+        return playlistRepository.findById(id);
+    }
+
+    public List<Playlist> findAllByUserId(String userId){
+        return playlistRepository.findAllByUserId(userId);
+    }
+
+    public void deletePlaylistById(String id){
+        playlistRepository.deletePlaylistById(id);
+    }
+
+    public void addTrackToPlaylist(String id, Track track){
+        playlistRepository.addTrackToPlayList(id, track);
+    }
+
+    public void removeTrackFromPlaylist(String id, String trackId){
+        playlistRepository.removeTrackFromPlaylist(id, trackId);
+    }
+}
