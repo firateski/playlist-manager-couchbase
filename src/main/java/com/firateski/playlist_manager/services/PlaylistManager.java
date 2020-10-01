c44e30cb9c1870006f4e0c66f7e32a4f30e5ac1d
Fırat Eşki <frat888@gmail.com>
2020-10-01 03:22:16 +0300

    Create interface for playlist manager

---
+++ src/main/java/com/firateski/playlist_manager/services/PlaylistManager.java
@@ -0,0 +1,20 @@
+package com.firateski.playlist_manager.services;
+
+import com.firateski.playlist_manager.models.Playlist;
+import com.firateski.playlist_manager.models.Track;
+
+import java.util.List;
+
+public interface PlaylistManager {
+    void create(Playlist playlist);
+
+    Playlist findById(String id);
+
+    List<Playlist> findAllByUserId(String userId);
+
+    void deletePlaylistById(String id);
+
+    void addTrackToPlaylist(String id, Track track);
+
+    void removeTrackFromPlaylist(String id, String trackId);
+}
